715cea2f1af4d2e38dd89ce8f2e31e9a252b2f04
RealFatCat <trashmessage@yandex.ru>
2022-09-16 19:57:53 +0300

    Don't run calc twice for 'restore burned cards' abillity

--- pkg/calc/calc.go
+++ pkg/calc/calc.go
@@ -12,21 +12,25 @@ func Rounds(m *merchants.Merchant) *Answer {
 }
 
 func calcRounds(m merchants.Merchanter) (s, l uint8) {
-	s, l = calc(m.HandSize())
-	if m.RestoreBurnedAbility() {
-		st, lt := calc(m.HandSize() - 1)
-		s += st
-		l += lt
-	}
+	s, l = calc(m.HandSize(), m.RestoreBurnedAbility())
 	return
 }
 
-func calc(hs uint8) (s, l uint8) {
-	for i := hs; i >= 2; i-- {
-		s += i / 2
-		if i > 2 {
-			l++
+func calc(hs uint8, ub bool) (s, l uint8) {
+	var f uint8                // number of rounds before first rest
+	for i := hs; i >= 2; i-- { // stop, when it just one card on hand left
+		s += i / 2 // player must use two cards, so first rest will be after the "number of cards in hand divided by two" round
+		if ub && i == hs {
+			f = s // save first result here, we need it only if merchant have "restore all burned cards" card
 		}
+		if i > 2 {
+			l++ // long rest gives one more round to play
+		}
+	}
+
+	if ub { // if merchant can restore it's burned cards
+		s = 2*s - f
+		l = 2*l - 1
 	}
 	return
 }
